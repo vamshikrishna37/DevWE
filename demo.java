ec4280a1c740375cb66a9409548fcade611dd25c
vamshikrishna37 <vamshikrishna5111@gmail.com>
2019-11-11 05:57:53 +0000

    demo.java

--- demo.java
+++ demo.java
@@ -1,10 +1,19 @@
-class add
+class sub()
+{
+psvm()
+{
+int a=8,b=6,c;
+c=a-b;
+s.o.p(c);
+}
+}
+class add()
 {
 psvm (string args())
 {
 int a=10,b=5,c;
 c=a+b;
-system.out print(c);
+system.out.print(c);
 }
 }
 
